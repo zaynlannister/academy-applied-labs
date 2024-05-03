34bf0577d500f4dca43a6b5e596d2b49fdf94b40
zaynlannister <zaynlannister@gmail.com>
2024-05-03 15:07:48 +0500

    feat: add featured blog

--- assets/swiper.js
+++ assets/swiper.js
@@ -1,16 +1,30 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
 
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".testimonials-swiper", {
   cssMode: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  pagination: {
-    el: ".swiper-pagination",
-  },
   mousewheel: true,
   keyboard: true,
 });
 
-console.log("sdd");
+const blogSwiper = new Swiper(".blog-swiper", {
+  slidesPerView: 3,
+  pagination: {
+    el: ".blog-swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    200: {
+      slidesPerView: 1,
+    },
+    770: {
+      slidesPerView: 2,
+    },
+    1120: {
+      slidesPerView: 3,
+    },
+  },
+});
