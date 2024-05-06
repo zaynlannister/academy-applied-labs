8dc6090b6f04ab3b9569b587bc8fca9b275e5d68
zaynlannister <zaynlannister@gmail.com>
2024-05-06 11:45:14 +0500

    feat: add dynamic logic to product page

---
+++ assets/product-section.js
@@ -0,0 +1,53 @@
+const productFormInput = document.querySelector("#product-form-input");
+const datasetUrl = document.querySelector("#dataset-url");
+
+const productVariants = document.querySelectorAll(".product-variant");
+
+function setDefaultVariant() {
+  const defaultId = productFormInput.value;
+
+  if (defaultId) {
+    const currentUrl = datasetUrl.getAttribute("data-url");
+
+    urlReplace(currentUrl, productFormInput.value);
+
+    // searching active variant by using url address
+    productVariants.forEach((item) => {
+      const variantId = item.getAttribute("data-id");
+      const urlParams = new URLSearchParams(window.location.search);
+      const currentVariantId = urlParams.get("variant");
+
+      setListenersToAllVariants(item);
+
+      if (currentVariantId == variantId) {
+        setVariantClass(item);
+      }
+    });
+  }
+}
+
+// setting class active on choosen variant
+function setVariantClass(activeVariant) {
+  productVariants.forEach((item) => {
+    item.classList.remove("active");
+  });
+  activeVariant.classList.add("active");
+}
+
+// url replacing function
+function urlReplace(currentUrl, value) {
+  window.history.replaceState({}, "", `${currentUrl}?variant=${value}`);
+}
+
+function setListenersToAllVariants(item) {
+  item.addEventListener("click", () => {
+    const currentUrl = datasetUrl.getAttribute("data-url");
+    const itemId = item.getAttribute("data-id");
+    urlReplace(currentUrl, itemId);
+
+    setVariantClass(item);
+    productFormInput.value = itemId;
+  });
+}
+
+setDefaultVariant();
