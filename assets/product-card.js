93b043a53bc9a7430b559d6b81b89dc107bfba47
zaynlannister <zaynlannister@gmail.com>
2024-05-17 17:41:22 +0500

    feat: add product card ATC menu & functionality

---
+++ assets/product-card.js
@@ -0,0 +1,44 @@
+const dropdownCardButtons = document.querySelectorAll(
+  ".product-card__form-dropdown"
+);
+const productCardVariants = document.querySelectorAll(
+  ".product-card__form-wrapper"
+);
+
+dropdownCardButtons.forEach((item) => {
+  item.addEventListener("click", toggleVariant);
+});
+
+function toggleVariant() {
+  const dataId = this.getAttribute("data-id");
+  const variantWrapper = document.querySelector(
+    `#product-card__form-wrapper-${dataId}`
+  );
+
+  this.classList.toggle("active");
+  variantWrapper.classList.toggle("active");
+}
+
+const allVariants = document.querySelectorAll(".product-card__form-variant");
+
+allVariants.forEach((variant) => {
+  variant.addEventListener("click", () => {
+    const id = variant.getAttribute("custom-id");
+    const clickedWrapper = document.querySelectorAll(
+      `#product-card__form-wrapper-${id} .product-card__form-variant`
+    );
+
+    const input = document.querySelector(`#product-card__form-input-${id}`);
+
+    removeActiveVariants(clickedWrapper);
+
+    variant.classList.add("active");
+    input.value = variant.getAttribute("data-id");
+  });
+});
+
+function removeActiveVariants(variants) {
+  variants.forEach((item) => {
+    item.classList.remove("active");
+  });
+}
